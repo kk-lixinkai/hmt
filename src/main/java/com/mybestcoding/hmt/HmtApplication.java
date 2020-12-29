8e0eb061c8c9168ccf2e29d44ee8978f1f145b60
kk-lixinkai <1976586654@qq.com>
2020-12-29 13:50:41 +0800

    创建了一个新项目

---
+++ src/main/java/com/mybestcoding/hmt/HmtApplication.java
@@ -0,0 +1,13 @@
+package com.mybestcoding.hmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmtApplication.class, args);
+	}
+
+}
